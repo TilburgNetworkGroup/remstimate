5176dbbbbf4368c56d252e2a90ea0f9a9a1accc9
jupepis <peppe949@gmail.com>
2020-04-20 11:15:37 +0200

    first commit (only three functions are committed: remCpp, lpd and nllik)

---
+++ R/remCpp.R
@@ -0,0 +1,46 @@
+#' remCpp  
+#'
+#' A function that returns maximum likelihood estimates of REM (interval timing only) by using the optim function
+#'
+#' @param stats an array of dimensions n_dyads*statistics*M (if stats is an array for relevent::rem then use aperm(a = stats, perm = c(2,3,1)) )
+#' @param event_binary a matrix of dimensions M*n_dyads (for each time point it is a vector with 1 when the dyad is observed 0 for the others)
+#' @param interevent_time a vector of interevent times 
+#' @param threads number of cores to use in the parallelization (i would change this parameter in 'n_threads')
+#'
+#' @return  object list (the function saves also the output of optim)
+remCpp <- function(pars_init = NULL,
+                    stats, 
+                    event_binary, 
+                    interevent_time, 
+                    threads, 
+                    method = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN", "Brent")){
+
+    # some check before running the optimization
+    if(length(method)>1){stop("only ONE method can be specified")}
+    else{if(!method%in%c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN",
+                 "Brent")){stop("the method defined is not one of those supported by optim(). Read optim() documentation (?optim)")}
+                 }
+    if(is.null(pars_init)) pars_init <- rep(0,dim(stats)[2])
+    else{ if(length(pars_init) != dim(stats)[2]) stop("length of initial parameters vector and number of columns of the array stats must be the same")}
+    
+    # optimization
+    ott <- optim(par = pars_init,
+                 fn = nllik,
+                 method = method,
+                 stats = stats,
+                 event_binary = event_binary,
+                 interevent_time = interevent_time,
+                 hessian = TRUE,
+                 n_cores = threads)
+
+    # storing output
+    out <- list()
+    out$coef <- ott$par # MLE estimates
+    out$hessian <- ott$hessian # hessian matrix
+    out$llik <- -ott$value # loglikelihood value at MLE values (we take the '-value' because we minimized the '-loglik')
+    out$AIC <- 2*length(out$coef) - 2*out$llik # AIC
+    out$BIC <- length(out$coef)*log(length(interevent_time)) - 2*out$llik # BIC
+    out$optim_output <- ott # optim output (if more information are needed)
+
+    return(out)
+}
---
+++ src/utils.cpp
@@ -0,0 +1,84 @@
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+#include <iostream>
+#include <omp.h>
+#include <RcppArmadilloExtensions/sample.h>
+#include <typeinfo>
+#include <map>
+#include <iterator>
+#include <string>
+
+
+
+#define LOG(x) std::cout << x << "\n"
+
+//  BEGIN Preprocessing functions //
+
+//' lpd (Log-Pointwise Density of REM)
+//'
+//' @param pars is a vector of parameters (note: the order must be aligned witht the column order in 'stats')
+//' @param stats is a matrix of dimensions n_dyads*variables with statistics of interest by column and dyads by row.
+//' @param event is a vector of 1/0 : 1 indicating the observed dyad and 0 the non observed dyads.
+//' @param interevent_time the time difference between the current time point and the previous event time.
+//'
+//' @return log-pointwise density value of a specific time point
+//'
+//' @export
+// [[Rcpp::export]]
+double lpd(arma::vec pars, arma::mat stats, arma::uvec event, double interevent_time){
+        arma::uword n_dyads = event.n_elem;
+        arma::uword i;
+        arma::vec log_lambda = stats * pars;
+        double lpd = 0.0;
+        for(i = 0; i < n_dyads; i++){
+            if(event(i) == 0){
+                lpd -= exp(log_lambda(i))*interevent_time;
+            }
+            else{
+                lpd += log_lambda(i)-exp(log_lambda(i))*interevent_time;
+            }
+        }
+        return lpd;
+    }
+
+//' nllik (Negative Log-Likelihood of REM)
+//'
+//' @param pars is a vector of parameters (note: the order must be the same as the column order in 'stats') at which to calculate the likelihood value
+//' @param stats is a cube of dimensions n_dyads*variables*M with statistics of interest by column and dyads by row.
+//' @param event_binary is a matrix of ones and zeros of dimensions M*n_dyads : 1 indicating the observed dyad and 0 the non-observed dyads.
+//' @param interevent_time the vector of time differences between the current time point and the previous event time (note: interevent time at t_1 is t_1-0=t_1, since t_0 = 0).
+//' @param threads
+//'
+//' @return negative log likelihood value
+//'
+//' @export
+// [[Rcpp::export]]  
+double nllik(arma::vec pars, 
+            arma::cube stats, 
+            arma::umat event_binary, 
+            arma::vec interevent_time,
+            int threads){
+
+        arma::uword n_dyads = event_binary.n_cols;
+        arma::uword i,m;
+        arma::uword M = event_binary.n_rows;
+        arma::vec log_lambda(n_dyads,arma::fill::zeros) ;
+        arma::vec llik(M,arma::fill::zeros);
+
+        omp_set_dynamic(0);           // disabling dynamic teams 
+        omp_set_num_threads(threads); // number of threads for all consecutive parallel regions
+        #pragma omp parallel for private(m,i,log_lambda) shared(n_dyads,M,stats,event_binary,interevent_time,llik)
+        for(m = 0; m < M; m++)
+        {
+            log_lambda = stats.slice(m) * pars;
+            for(i = 0; i < n_dyads; i++){
+                if(event_binary(m,i) == 0){
+                    llik(m) -= exp(log_lambda(i))*interevent_time(m);
+                }
+                else{
+                    llik(m) += log_lambda(i)-exp(log_lambda(i))*interevent_time(m);
+                }
+            }
+        }
+        return -sum(llik);
+    }
